a76a9a730ab76a0140f7bf2887079e32e6c78c96
Markus Holtermann <markus@markusholtermann.eu>
2011-12-10 22:03:01 +0100

    add http request methods HEAD and OPTIONS

--- src/main/java/org/scribe/model/Verb.java
+++ src/main/java/org/scribe/model/Verb.java
@@ -3,9 +3,12 @@ package org.scribe.model;
 /**
  * An enumeration containing the most common HTTP Verbs.
  * 
- * @author Pablo Fernandez
+ * As defined by the <a
+ * href="http://tools.ietf.org/html/rfc2616.html#section-5.1.1">RFC 2616 in
+ * section 5.1.1</a>. <code>CONNECT</code> and <code>TRACE</code> are ignored.
+ * 
+ * @author Pablo Fernandez, Markus Holtermann
  */
-public enum Verb
-{
-  GET, POST, PUT, DELETE
+public enum Verb {
+	GET, POST, PUT, DELETE, HEAD, OPTIONS;
 }
